1518fa8ca39f95c600c9e35758bcecc1e3f627aa
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-02 15:26:08 +0200

    Added the Node class and constructor method

--- 14Practical/src/chainedHash.java
+++ 14Practical/src/chainedHash.java
@@ -1,2 +1,23 @@
 public class chainedHash {
+
+    private static class Node {
+        String key;
+        String value;
+        Node next;
+
+        Node(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+    private Node[] table;
+    private  int m;
+    private int size;
+
+    public chainedHash(int m ){
+        this.m = m;
+        this.table = new Node[m];
+        this.size = 0;
+    }
 }
