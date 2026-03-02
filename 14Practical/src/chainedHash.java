1b380716ad00c1f703f01f9124db5d319f8716fe
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-02 15:34:16 +0200

    added insert method in the chainedHash class

--- 14Practical/src/chainedHash.java
+++ 14Practical/src/chainedHash.java
@@ -27,6 +27,31 @@ public class chainedHash {
         return h % m;
     }
 
-    
+    public void insert(String key,String value) {
+        int index = hash(key);
+        Node head = table[index];
+
+        if(head == null){
+            table[index] = new Node(key,value);
+            size++;
+            return;
+        }
+
+        Node current = head;
+        Node prev = null;
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                current.value = value;
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+        prev.next = new Node(key,value);
+        size++;
+    }
+
+
 
 }
