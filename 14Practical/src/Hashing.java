fe13e4e0ceacb4d1eb1c1e1116464b450921e50b
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-02 14:43:41 +0200

    Set up array key-value pairs as data done

--- 14Practical/src/Hashing.java
+++ 14Practical/src/Hashing.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Hashing {
+    static class KeyValue {
+        String key;
+        String value;
+
+        KeyValue(String key,String value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int N = 1 << 20;
+        List<Integer> keys = new ArrayList<>(N);
+
+        for (int i = 0; i < N ; i++) {
+            keys.add(i);
+        }
+
+        //Shuffling
+        for (int i = 0; i < N; i++) {
+            int randomNumber = i + random.nextInt(N-i);
+
+            Integer temp = keys.get(i);
+            keys.set(i, keys.get(randomNumber));
+            keys.set(randomNumber, temp);
+        }
+
+        KeyValue[] data = new KeyValue[N];
+
+        for (int i = 0; i < N; i++) {
+           String key = String.valueOf(keys.get(i));
+           String value = String.valueOf(i+1);
+           data[i] = new KeyValue(key,value);
+        }
+
+        System.out.printf("Genrated %d key values pairs.",N);
+
+    }
 }
