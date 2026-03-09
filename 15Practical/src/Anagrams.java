027680d7230b7baa4a812f6b9c0cde6e411c437d
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-09 14:28:43 +0200

    Cleaning the words and adding a Dicintary using a hashmap

--- 15Practical/src/Anagrams.java
+++ 15Practical/src/Anagrams.java
@@ -3,7 +3,9 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Anagrams {
     public static String signature(String word){
@@ -12,7 +14,7 @@ public class Anagrams {
     return new String(letters); // rejoining them into string
    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filename = "ulysses.text"; // Making a varable to store the path of the given textfile
         BufferedReader fileReader;
         try {
@@ -20,8 +22,24 @@ public class Anagrams {
             System.out.println("Opened the file: " + filename);
         } catch (FileNotFoundException e) { // catching the eroor if the file is not found in the floder
             System.out.println("File not found");
-
+            return;
         }
+
+        HashMap<String,Integer> D = new HashMap<>();
+            String line;
+        while ((line = fileReader.readLine()) != null){
+                String[] words = line.split("\\s"); // any chreacter thats not in a-zA-z
+                for (String w : words){
+                    String cleaned = w.replaceAll("[^a-zA-z']","");
+                    cleaned = cleaned.toLowerCase(); // makes it lower case
+                    if (cleaned.isEmpty()) continue;
+                    D.put(cleaned, D.getOrDefault(cleaned, 0) + 1); // added one if its a new word
+                }
+            }
+            fileReader.close();
+            System.out.println("Total unique words found: " + D.size());
+
+
     }
 
 
