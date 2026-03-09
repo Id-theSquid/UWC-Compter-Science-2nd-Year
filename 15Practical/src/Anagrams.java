2424dd90e4f70f72a66b8ce94937cd454b008a6d
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-09 14:08:59 +0200

    Added the signature method to java class

--- 15Practical/src/Anagrams.java
+++ 15Practical/src/Anagrams.java
@@ -1,3 +1,12 @@
+//Idwou Popoola
+// 4546626
+import java.util.Arrays;
+
 public class Anagrams {
 
+    public static String signature(String word){
+    char[] letters = word.toCharArray(); // breaking word into characters
+    Arrays.sort(letters); // sorting them aplhabeticalli order
+    return new String(letters); // rejoining them into string
+   }
 }
