3a9566424af32fc7ab6b102d4d27d8c9c60b7111
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-16 14:58:58 +0200

    Did a redo first implenation was wrong didnt include nodes like how the slides and notes did it ,added a heapify method in the static class called heap

--- 16practical/src/tryHeapsort.java
+++ 16practical/src/tryHeapsort.java
@@ -1,18 +1,42 @@
 public class tryHeapsort {
-    private static int parent(int i) {
-        return (i - 1) / 2;
+    static class Node {
+        String key;
+        Node(String key) { this.key = key; }
+        int compareTo(Node other) {
+            return this.key.compareToIgnoreCase(other.key);
+        }
+        @Override
+        public String toString() { return key; }
     }
-    private static int leftChild(int i) {
-        return 2 * i + 1;
+
+    static class Heap {
+        Node[] A;
+        int n;
+        private int parent (int i){
+            return i / 2;
+        }
+        private int left (int i) {
+            return 2*i;
+        }
+        private void swap(int i,int j) {
+            Node temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+        public void heapify(int i) {
+            int child = left(i);
+            while(child <= n) {
+                if (child < n && A[child].compareTo(A[child + 1]) < 0)
+                    child++;
+                if (A[child].compareTo(A[i]) > 0) {
+                    swap(i, child);
+                    i     = child;
+                    child = left(i);
+                } else {
+                    break;
+                }
+            }
+        }
     }
-    private static int rightChild(int i) {
-        return 2 * i + 2;
-    }
-    public static void swap(String[] a,int i,int j){
-        String temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-   
 }
 
