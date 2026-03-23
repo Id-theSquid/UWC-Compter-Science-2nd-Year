db042f0421b97015687826708f910b854cb6d322
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-23 15:00:50 +0200

    Aded Class Tree,insert method,checking bst,remobing ndoe method

--- 17practical/src/tryBST.java
+++ 17practical/src/tryBST.java
@@ -1,13 +1,65 @@
+import org.w3c.dom.Node;
+
+import java.security.Key;
+
 //idowu Popoola
-//Comsuluted Claude
+//Comsuluted Claude(free version)
 class tNode{
     int key;
-    tNode left,right;
-    tNode(int key) {
+    tNode left,right,parent;
+    public tNode(int key) {
         this.key = key;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 }
-public class tryBST {
+class Tree {
+    tNode root;
+    public Tree() {
+        root = null;
+    }
+    public void insert(int key){
+        root = insertRec(root,null,key);
+    }
+    private tNode insertRec(tNode node,tNode parent,int key){
+        if (node == null){
+            tNode newNode = new tNode(key);
+            newNode.parent = parent;
+            return newNode;
+        }
+        if (key < node.key)
+            node.left = insertRec(node.left,node,key);
+        else
+            node.right = insertRec(node.right,node,key);
+
+        return node;
+    }
+    //Cheching bST
+    public boolean isBST(){
+        return isBSTRec(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+    private boolean isBSTRec(tNode node,int min,int max){
+        if (node == null){
+            return true;
+        }
+        if (node.key <= min || node.key >= max){
+            return false;
+        }
+        return isBSTRec(node.left,min,node.key) && isBSTRec(node.right,node.key,max);
+    }
+    //Removing Node
+    public void remove(int key){
+        root= removeRec(root,key);
+    }
+    private tNode removeRec(tNode node,int key){
+        if (node == null){
+            return null;
+        }
+        if (key < node.key){
+            node.left = removeRec(node.left,key);
+        }
+        return isBSTRec(node.left,min,node.key) && isBSTRec(node.right,node.key,max);
+    }
+
 }
