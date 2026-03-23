48875396f78598c700ef298d4925820058991a7d
Id-theSquid <idowupopoolaa@gmail.com>
2026-03-23 14:20:44 +0200

    Added Node

--- 17practical/src/tryBST.java
+++ 17practical/src/tryBST.java
@@ -1,2 +1,13 @@
+//idowu Popoola
+//Comsuluted Claude
+class tNode{
+    int key;
+    tNode left,right;
+    tNode(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
 public class tryBST {
 }
